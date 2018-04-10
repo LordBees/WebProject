66e2b68c65727ebe63375d59601e2389bf3111aa
Luke Clayton <luke2.clayton@live.uwe.ac.uk>
2018-04-11 00:23:27 +0100

    BABY

--- webTrain.py
+++ webTrain.py
@@ -311,7 +311,7 @@ def createTrainForm(depart_location,arrive_location,passenger_count,dtime,depart
 		if(int(passenger_count) > 0):
  			passCntMin= 1
  		else:
- 			pasCntMin=0
+ 			passCntMin=0
 		
 		# previous issue here was checking "" when they were sometimes set to "--", its consistent now for "--"
 		if(depart_location != "--" and arrive_location != "--"): 
